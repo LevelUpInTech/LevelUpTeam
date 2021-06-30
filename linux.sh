62db06f0de8932d4d94618ecbd26f9014c1e951d
pax21 <60755833+pax21@users.noreply.github.com>
2021-06-29 23:00:05 -0500

    "updated linux.sh"

--- linux.sh
+++ linux.sh
@@ -5,17 +5,17 @@ read Paxton Gwinn
 echo "Reading.... please wait"
 sleep 3 
 echo "-----------------------------"
-echo "Your username is:" && whoami 
+echo "Your username is:" pax21 
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
-echo "Made on:" && date
+echo "Made on:" 06/29/21
 echo "-----------------------------"
 sleep 2
 echo "My IP address is:"
-echo "-----------------------------"
+echo "34.214.8.168"
 sleep 2
 curl ifconfig.me
 
