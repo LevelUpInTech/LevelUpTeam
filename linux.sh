9d78210442dace4f155eb40a83d600e46a7eab6c
CaptainYeezy <zackiechan2001@yahoo.com>
2021-10-05 20:29:22 +0000

    In this repo I have change a few words using vim.
    I started off by forking my given repo and copying the forked repo.
    
    Afterwards, I checked to ensure my folder was available with the doc.
    
    Once that was finished, I changed my directory and edited my file with VIM.
    
    After completion, I saved my file and added it to the repo.
    
    Now I am committing the file and afterwards I will be pushing it back.

--- linux.sh
+++ linux.sh
@@ -1,20 +1,21 @@
+
 #!/bin/bash
 
-echo "Please enter your name:"
+echo "Please enter your name: Zach"
 read name
 echo "Reading.... please wait"
 sleep 3 
 echo "-----------------------------"
-echo "Your username is:" && whoami 
+echo "Your username is: CaptainYeezy" && whoami 
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
-echo "Made on:" && date
+echo "Made on: October 3, 2021" && date
 echo "-----------------------------"
 sleep 2
-echo "My IP address is:"
+echo "My IP address is: 18.144.13.6"
 echo "-----------------------------"
 sleep 2
 curl ifconfig.me
