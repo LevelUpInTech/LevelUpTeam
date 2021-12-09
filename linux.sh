93639028b0a08a6551140e33c7c5ba350c51a641
DeQuay Glascoe <dmglascoe@protonmail.com>
2021-12-08 20:35:08 -0600

    Just another test for a screenshot

--- linux.sh
+++ linux.sh
@@ -5,7 +5,7 @@ read "DeQuay Glascoe"
 echo "Reading.... please wait"
 sleep 3 
 echo "-----------------------------"
-echo "Your username is:" && whoami 
+echo "Your username is :" && whoami 
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
