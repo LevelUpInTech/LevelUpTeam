8d65fc84d2518ded8b2f1a223f2d1d0f2e1b3e16
Naomi Legranvil <nttlegranvil@gmail.com>
2022-01-31 01:03:34 +0000

    added information to linux.sh

--- linux.sh
+++ linux.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 echo "Please enter your name:"
-read name
+read Naomi Legranvil
 echo "Reading.... please wait"
 sleep 3 
 echo "-----------------------------"
-echo "Your username is:" && whoami 
+echo "Your username is:" NtLegranvil
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
-echo "Made on:" && date
+echo "Made on:" 1/30/2022
 echo "-----------------------------"
 sleep 2
-echo "My IP address is:"
+echo "My IP address is:" 35.85.215.189
 echo "-----------------------------"
 sleep 2
 curl ifconfig.me
