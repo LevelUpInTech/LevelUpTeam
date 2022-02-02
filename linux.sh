3d43c50bf2addf52bd9c79e39ca7fe51ed54b216
JarvisB92 <jarvis.bryant90@gmail.com>
2022-02-02 02:09:11 +0000

    linux.sh

--- linux.sh
+++ linux.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+
 echo "Please enter your name:"
 read name
 echo "Reading.... please wait"
-sleep 3 
+sleep 5 
 echo "-----------------------------"
 echo "Your username is:" && whoami 
 echo "-----------------------------"
