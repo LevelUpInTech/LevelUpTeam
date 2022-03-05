0c7d58936de7c013dc2c1281b56079d3ae427f9e
halenoy <halenoy@email.com>
2022-03-05 03:23:55 +0000

    halenoy made changes to the linux.sh file

--- linux.sh
+++ linux.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Please enter your name:"
+halenoy
 read name
 echo "Reading.... please wait"
 sleep 3 
@@ -8,6 +9,7 @@ echo "-----------------------------"
 echo "Your username is:" && whoami 
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
+This is my first github project
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
@@ -20,3 +22,5 @@ sleep 2
 curl ifconfig.me
 
 This is week 4 - project test
+Hale is completing her first github project while drinking lemonade
+
