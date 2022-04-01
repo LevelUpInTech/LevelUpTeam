9674a370bf55bd76fef048d31786531c335f4beb
award0624 <award0624@gmail.com>
2022-04-01 16:37:29 +0000

    april added new text

--- linux.sh
+++ linux.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+This was a fun project to do 
 
-echo "Please enter your name:"
-read name
+echo Please enter your name:"
+ 
 echo "Reading.... please wait"
 sleep 3 
 echo "-----------------------------"
 echo "Your username is:" && whoami 
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
+
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
