7cccf7086bd7599b605c80e59bc1aeab58dc6baa
bfreeland <brianfree66@yahoo.com>
2022-04-03 17:10:15 +0000

    brian added new changes to this file.

--- linux.sh
+++ linux.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-echo "Please enter your name:"
+echo "brian freeland:"
 read name
 echo "Reading.... please wait"
 sleep 3 
 echo "-----------------------------"
-echo "Your username is:" && whoami 
+echo "Your username is:" bfreeland56 
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
-echo "Made on:" && date
+echo "Made on:" April 3 2022
 echo "-----------------------------"
 sleep 2
-echo "My IP address is:"
+echo "My IP address is:"2601:150:4300:e320:e922:a3e0:7b23:d286
 echo "-----------------------------"
 sleep 2
 curl ifconfig.me
 
 This is week 4 - project test
+
+These are Brian's changes
