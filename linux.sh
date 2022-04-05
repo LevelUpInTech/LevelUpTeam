3663772ab5f67dd746d4fb908fa6dc42390beeeb
Daylon Lockett <lockettda23@gmail.com>
2022-04-05 23:03:59 +0000

    always last

--- linux.sh
+++ linux.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-echo "Please enter your name:"
+echo "Daylon Lockett:"
 read name
 echo "Reading.... please wait"
 sleep 3 
 echo "-----------------------------"
-echo "Your username is:" && whoami 
+echo "DaylonL:" && whoami 
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
-echo "Made on:" && date
+echo "4/5/22:" && date
 echo "-----------------------------"
 sleep 2
-echo "My IP address is:"
+echo "3.17.173.248"
 echo "-----------------------------"
 sleep 2
 curl ifconfig.me
