6f9509943052200c226de37bbfd3f62b8133c08a
Darcy Wood <darcyjwood@gmail.com>
2022-04-30 21:26:14 +0000

    added a greeting, added a friendly comment after name, added current directory information, changed the ip address into a variable, added a weather forcast, fixed the final comment to display correctly

--- linux.sh
+++ linux.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+whereami=$(pwd)
+weather=$(curl -s wttr.in | head -n 7)
+ipaddress=$(curl -s ifconfig.me)
+
+echo "Hello!"
+sleep 2
 echo "Please enter your name:"
 read name
 echo "Reading.... please wait"
 sleep 3 
+echo "It's nice to meet you!"
 echo "-----------------------------"
 echo "Your username is:" && whoami 
 echo "-----------------------------"
+sleep 2
+echo "You are currently in the directory:"
+echo "$whereami"
+echo "-----------------------------"
+sleep 2 
 echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
@@ -15,8 +27,11 @@ echo "Made on:" && date
 echo "-----------------------------"
 sleep 2
 echo "My IP address is:"
+echo "$ipaddress"
 echo "-----------------------------"
 sleep 2
-curl ifconfig.me
+echo "Here is your local $weather forcast."
+sleep 3
+echo "This is a week 4 project test for the awesome LUIT Red Team."
 
-This is week 4 - project test
+
