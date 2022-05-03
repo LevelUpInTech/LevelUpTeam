10a127584de7cee85fae4aa3b1fcc82844949108
GitHub K.Price <dizzlekp06@gmail.com>
2022-05-03 23:59:08 +0000

    Added week 4 message

--- linux.sh
+++ linux.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo "Please enter your name:"
+echo "Kendrick:"
 read name
 echo "Reading.... please wait"
 sleep 3 
 echo "-----------------------------"
-echo "Your username is:" && whoami 
+echo "Your username is:" dizzlekp 
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
-echo "Made on:" && date
+echo "Made on:" 05/03/2022
 echo "-----------------------------"
 sleep 2
 echo "My IP address is:"
