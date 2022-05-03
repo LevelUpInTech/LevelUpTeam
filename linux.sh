ded8a608c407e7a0ae4d29099496e4a265ffc10b
TylerDidIt <TylerDidIt@gmail.com>
2022-05-02 22:22:14 -0400

    Made a change to Wk 4 Project 1

--- linux.sh
+++ linux.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+# !/bin/bash
 
 echo "Please enter your name:"
 read name
@@ -19,4 +19,5 @@ echo "-----------------------------"
 sleep 2
 curl ifconfig.me
 
-This is week 4 - project test
+This is week 4 - project test  
+Tyler was here for the week 4 Project!
