b4dcd719738f969e04300ad93b54bc0866c1b7fc
Brian Moore <brianhawkmoore@gmail.com>
2022-07-02 18:09:00 +0000

    bmoore changes

--- linux.sh
+++ linux.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 echo "Please enter your name:"
-read name
+read Brian Moore
 echo "Reading.... please wait"
 sleep 3 
 echo "-----------------------------"
-echo "Your username is:" && whoami 
+echo "Your username is:" Mohawk9140 
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
-echo "Made on:" && date
+echo "Made on:" 7/02/2022
 echo "-----------------------------"
 sleep 2
 echo "My IP address is:"
 echo "-----------------------------"
 sleep 2
-curl ifconfig.me
+18.116.50.84
 
 This is week 4 - project test
