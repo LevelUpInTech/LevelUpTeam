cc8f5a833eb23c140dba80a1183e8c846f616f36
Joseph Peter <josephvpeter7@gmail.com>
2022-09-02 03:24:29 +0000

    print the curl command and last line

--- linux.sh
+++ linux.sh
@@ -18,5 +18,6 @@ echo "My IP address is:"
 echo "-----------------------------"
 sleep 2
 curl ifconfig.me
-
-This is week 4 - project test
+echo -e "\n-------------------------------"
+echo "This is week 4 - project test"
+echo "------------------------------"
