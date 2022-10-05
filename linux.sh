14efb128edb3605037088f528692aa3932afc7a4
theCaptN21 <khenr89@wgu.edu>
2022-10-05 21:36:05 +0000

    Made Changes to linux.sh

--- linux.sh
+++ linux.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-echo "Please enter your name:"
+echo "Katoria:"
 read name
 echo "Reading.... please wait"
 sleep 3 
 echo "-----------------------------"
-echo "Your username is:" && whoami 
+echo "Katoria:" && whoami 
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
-echo "Made on:" && date
+echo "October 5, 2022:" && date
 echo "-----------------------------"
 sleep 2
-echo "My IP address is:"
+echo "123.45.678.90:"
 echo "-----------------------------"
 sleep 2
 curl ifconfig.me
 
 This is week 4 - project test
+
+
