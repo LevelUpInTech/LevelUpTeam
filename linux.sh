768a86434e02930592bc15cb1ba07c98e4a4c079
Candace Hollinger <csig1985@gmail.com>
2022-10-08 16:48:36 +0000

    CanHoll made changes to linux.sh

--- linux.sh
+++ linux.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 echo "Please enter your name:"
-read name
+read Hi there this is Candace Hollinger 
 echo "Reading.... please wait"
 sleep 3 
 echo "-----------------------------"
-echo "Your username is:" && whoami 
+echo "Your username is:" my username is sade954
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
-echo "Made on:" && date
+echo "Made on:" 10/8/2022
 echo "-----------------------------"
 sleep 2
 echo "My IP address is:"
