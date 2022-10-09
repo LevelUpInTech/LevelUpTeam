fbfc964e1604bcc1b20476c58387b1231e4a6ad3
Burse97 <darious.burse@yahoo.com>
2022-10-09 20:40:45 +0000

    Dburse made changes to linux.sh

--- linux.sh
+++ linux.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 echo "Please enter your name:"
-read name
+read My name is Darious Burse
 echo "Reading.... please wait"
 sleep 3 
 echo "-----------------------------"
-echo "Your username is:" && whoami 
+echo "Your username is:"Burse97 && whoami 
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
-echo "Made on:" && date
+echo "Made on:"10/09/2022 && date
 echo "-----------------------------"
 sleep 2
-echo "My IP address is:"
+echo "My IP address is:34.220.96.197"
 echo "-----------------------------"
 sleep 2
 curl ifconfig.me
