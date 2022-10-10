5b2439c9971c7e89912e06465cee3a1454ff58e1
Josh <Josh@levelupintech.com>
2022-10-10 01:16:28 +0000

    Removed linux.sh delay

--- linux.sh
+++ linux.sh
@@ -3,7 +3,7 @@
 echo "Please enter your name:"
 read name
 echo "Reading.... please wait"
-echo "-----------------------------"
+cho "-----------------------------"
 echo "Your username is:" && whoami 
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
