2bf728687343dd185c2f8df5973d71d8c48b86bc
JarvisCarter <jarviscarter38@gmail.com>
2022-10-11 23:38:51 +0000

    Jarvis Changed

--- linux.sh
+++ linux.sh
@@ -1,20 +1,22 @@
+
+
 #!/bin/bash
 
 echo "Please enter your name:"
-read name
+read Jarvis
 echo "Reading.... please wait"
 sleep 3 
 echo "-----------------------------"
-echo "Your username is:" && whoami 
+echo "Your username is:" JarvisCarter 
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
-echo "Made on:" && date
+echo "Made on:" 10/11/2022
 echo "-----------------------------"
 sleep 2
-echo "My IP address is:"
+echo "My IP address is:"34.222.144.166
 echo "-----------------------------"
 sleep 2
 curl ifconfig.me
