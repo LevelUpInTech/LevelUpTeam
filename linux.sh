034421af1c473d8e5a7312e1aa7d43d3dc7cbac7
AWRozier <alexuswoods@gmail.com>
2022-10-13 22:40:29 +0000

    AWRozier made a few changes

--- linux.sh
+++ linux.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-echo "Please enter your name:"
+echo "Alexus Rozier:"
 read name
 echo "Reading.... please wait"
 sleep 3 
 echo "-----------------------------"
-echo "Your username is:" && whoami 
+echo "Your username is:" AWRozier 
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
-echo "Made on:" && date
+echo "Made on:" 10/13/2022
 echo "-----------------------------"
 sleep 2
-echo "My IP address is:"
+echo "My IP address is:18.217.182.137 "
 echo "-----------------------------"
 sleep 2
 curl ifconfig.me
