180c0d59094289e85c818279c70f732c53c75010
Shay Sicky <Shaysicky@ymail.com>
2022-10-29 15:36:02 +0000

    updated file

--- linux.sh
+++ linux.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 echo "Please enter your name:"
-read name
+read Shay
 echo "Reading.... please wait"
 sleep 3 
 echo "-----------------------------"
-echo "Your username is:" && whoami 
+echo "Your username is:" && cloud_user 
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
-echo "Made on:" && date
+echo "Made on:" && Oct28
 echo "-----------------------------"
 sleep 2
 echo "My IP address is:"
@@ -20,3 +20,4 @@ sleep 2
 curl ifconfig.me
 
 This is week 4 - project test
+
