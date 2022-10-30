865df2aaccf11687d6bbc701a2360273e7f83cbd
Jojo <jshenry91@yahoo.com>
2022-10-30 00:33:09 +0000

    Added black team and echo at the bottom

--- linux.sh
+++ linux.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 echo "Please enter your name:"
-read name
+read Jojo
 echo "Reading.... please wait"
-sleep 3 
+sleep 3
 echo "-----------------------------"
-echo "Your username is:" && whoami 
+echo "Your username is:" cloud_user 
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
-echo "Made on:" && date
+echo "Made on:" 10-29-22
 echo "-----------------------------"
 sleep 2
 echo "My IP address is:"
-echo "-----------------------------"
+echo "3.129.20.130-----------------------------"
 sleep 2
 curl ifconfig.me
 
-This is week 4 - project test
+echo "This is week 4 - project test:
