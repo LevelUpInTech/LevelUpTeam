41306dabc37c57826f077ec3e692a9553e8b740c
jojohenry <jshenry91@yahoo.com>
2022-10-31 03:40:33 +0000

    Added GitHub Project at bottom

--- linux.sh
+++ linux.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 echo "Please enter your name:"
-read Jojo
+read name
 echo "Reading.... please wait"
 sleep 3
 echo "-----------------------------"
-echo "Your username is:" cloud_user 
+echo "Your username is:" &&whoami 
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
-echo "Made on:" 10-29-22
+echo "Made on:" && date
 echo "-----------------------------"
 sleep 2
 echo "My IP address is:"
-echo "3.129.20.130-----------------------------"
+echo "-----------------------------"
 sleep 2
 curl ifconfig.me
-
-echo "This is week 4 - project test:
+echo "This is week 4 - project test: GitHub: Push, Pull & Fork"
