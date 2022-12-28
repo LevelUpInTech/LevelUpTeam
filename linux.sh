bd025315268f580a476d0772886dcd524ccf547a
Phillipe Williams <phillbrid@gmail.com>
2022-12-28 23:34:26 +0000

    Change file

--- linux.sh
+++ linux.sh
@@ -5,13 +5,13 @@ read Phillipe
 echo "Reading.... please wait"
 sleep 3 
 echo "-----------------------------"
-echo "Your username is:" && Philldanupe  
+echo "Your username is:"  Philldanupe  
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
-echo "Made on:" && 12-28-2022 
+echo "Made on:"  12-28-2022 
 echo "-----------------------------"
 sleep 2
 echo "My IP address is:"
