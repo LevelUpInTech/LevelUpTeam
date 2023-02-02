e0f14998bbb7978f6f5ad409f9a06ee76e47086e
muneebaden <muneebaden90@gmail.com>
2023-02-02 23:42:40 +0000

    LUIT Project 02: muneebaden

--- linux.sh
+++ linux.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 echo "Please enter your name:"
-read name
+read Muneeb Aden
 echo "Reading.... please wait"
 sleep 3 
 echo "-----------------------------"
-echo "Your username is:" && whoami 
+echo "Your username is: cloud_user" 
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
-echo "Made on:" && date
+echo "Made on: Thu Feb 2 18:25:22 2023" 
 echo "-----------------------------"
 sleep 2
-echo "My IP address is:"
+echo "My IP address is: 13.52.163.54"
 echo "-----------------------------"
 sleep 2
 curl ifconfig.me
 
-This is week 4 - project test
+# This is week 4 - project test
