b0ad06970e97b807c5cc793f0757c87e960b43fd
Victoria <cloud_user@c62da921e51c.mylabserver.com>
2023-02-04 19:12:07 +0000

    updated info in file

--- linux.sh
+++ linux.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-echo "Please enter your name:"
-read name
+echo "Please eiinter your name:"
+read Victoria Jackson
 echo "Reading.... please wait"
 sleep 3 
 echo "-----------------------------"
-echo "Your username is:" && whoami 
+echo "Your username is:" cloud_user 
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
-echo "Made on:" && date
+echo "Made on:" & Feb42023
 echo "-----------------------------"
 sleep 2
 echo "My IP address is:"
 echo "-----------------------------"
 sleep 2
 curl ifconfig.me
-
+52.33.9.191[cloud_user@c62da921e51c
 This is week 4 - project test
