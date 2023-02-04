8d27002f019580356d59d2955fa1f43319371feb
Sharon Johnson <Sharon@Luit.com>
2023-02-04 00:25:14 +0000

    modified linux.sh

--- linux.sh
+++ linux.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 echo "Please enter your name:"
-read name
+read Sharon
 echo "Reading.... please wait"
 sleep 3 
 echo "-----------------------------"
-echo "Your username is:" && whoami 
+echo "Your username is:" cloud_user 
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
-echo "Made on:" && date
+echo "Made on:" Feb 03 2023
 echo "-----------------------------"
 sleep 2
-echo "My IP address is:"
+echo "My IP address is:" 18.117.99.24
 echo "-----------------------------"
 sleep 2
 curl ifconfig.me
 
-This is week 4 - project test
+echo "This is week 4 - project Git and GitHub"
