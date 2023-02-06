fb36420a380ee1a4eda0df35eabf21203fb3d772
eddy moran <eddy@email.com>
2023-02-06 08:42:53 +0000

    Added the info requested

--- linux.sh
+++ linux.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-echo "Please enter your name:"
+echo "Eduardo Moran"
 read name
 echo "Reading.... please wait"
 sleep 3 
 echo "-----------------------------"
-echo "Your username is:" && whoami 
+echo "Your username is:"cloud_user 
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
-echo "Made on:" && date
+echo "Made on:" 2/6/2023
 echo "-----------------------------"
 sleep 2
-echo "My IP address is:"
+echo "My IP address is:"18.190.152.253
 echo "-----------------------------"
 sleep 2
 curl ifconfig.me
