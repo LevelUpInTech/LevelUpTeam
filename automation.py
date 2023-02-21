825836ae9015307522f1c3a9f6f1ad879900f244
EC2 Default User <ec2-user@ip-172-31-23-224.ec2.internal>
2023-02-21 23:13:00 +0000

    different ways to search for buckets

--- automation.py
+++ automation.py
@@ -1,11 +1,17 @@
 import boto3
 
 s3=boto3.resource('s3')
-bucket = s3.Bucket("darrylearnbucket")
-response = bucket.create(
-CreateBucketConfiguration = {
-        'LocationConstraint': 'us-east-2'
-    }
-    )
-print(response)
+#bucket = s3.Bucket("darrylearnbucket")
+#response = bucket.create(
+#CreateBucketConfiguration = {
+        #'LocationConstraint': 'us-east-2'
+  #  }
+  #  )
+#print(response)
 
+#bucketlist = list (s3.buckets.all())
+
+#print(bucketlist)
+
+for bucket in s3.buckets.all():
+    print (bucket)
