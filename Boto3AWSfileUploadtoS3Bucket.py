9f874a1d1a86a7d846a6bbc14022a5fc9d618ae9
Darius-Commits <darius.sage.marte@gmail.com>
2023-02-22 23:06:41 +0000

    upload a single file to s3 bucket

---
+++ Boto3AWSfileUploadtoS3Bucket.py
@@ -0,0 +1,10 @@
+import boto3
+#upload a single file to s3 bucket
+s3 = boto3.client("s3")
+
+s3.upload_file(
+    Filename = 'README.md', 
+    Bucket = 'darrylearnbucket', 
+    Key = 'uploadTest.md' , 
+    
+    )
