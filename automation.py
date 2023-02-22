bbc7c48f34fbc00871fda4a6b40dc4497ea43da3
Darius-Commits <darius.sage.marte@gmail.com>
2023-02-22 19:01:40 +0000

    use methods to create a dictionary of bucket data, then isolate desired data

--- automation.py
+++ automation.py
@@ -1,6 +1,25 @@
 import boto3
 
-s3=boto3.resource('s3')
+s3_resource = boto3.client("s3")
+
+# return a dictionary of buckets
+#s3_resource.list_buckets()
+
+#print first bucket in dictionary of buckets
+#print(s3_resource.list_buckets()['Buckets'][0])
+
+#print first bucket in dictionary of buckets
+#print(s3_resource.list_buckets()['Buckets'][0]['CreationDate'])
+
+bucketIndex=s3_resource.list_buckets()['Buckets']
+
+for bucket in bucketIndex: 
+    print(bucket["Name"])
+    print(bucket["CreationDate"])
+
+
+
+#s3=boto3.resource('s3')
 #bucket = s3.Bucket("darrylearnbucket")
 #response = bucket.create(
 #CreateBucketConfiguration = {
@@ -13,5 +32,5 @@ s3=boto3.resource('s3')
 
 #print(bucketlist)
 
-for bucket in s3.buckets.all():
-    print (bucket)
+#for bucket in s3.buckets.all():
+#print (bucket)
