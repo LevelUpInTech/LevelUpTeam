bd37fe239555e79c80cd06b380f25cfd2bbcb686
Darius-Commits <darius.sage.marte@gmail.com>
2023-02-23 19:42:58 +0000

    use glob to find all files

--- Boto3AWSfileUploadtoS3Bucket.py
+++ Boto3AWSfileUploadtoS3Bucket.py
@@ -1,10 +1,20 @@
 import boto3
-#upload a single file to s3 bucket
-s3 = boto3.client("s3")
+import os
+import glob
 
-s3.upload_file(
-    Filename = 'README.md', 
-    Bucket = 'darrylearnbucket', 
-    Key = 'uploadTest.md' , 
+cwd = os.getcwd()
+
+files = glob.glob("*")
+print (files)
+
+
+#upload a single file to s3 bucket
+#s3 = boto3.client("s3")
+
+#s3.upload_file(
+ #   Filename = 'README.md', 
+  
+  #  Bucket = 'darrylearnbucket', 
+   # Key = 'uploadTest.md' , 
     
-    )
+    #)
