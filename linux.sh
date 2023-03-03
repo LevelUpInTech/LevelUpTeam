42520ce867312bc0ce678797fd5d0dedd794baa6
Jim Flermoen <james.flermoen@gmail.com>
2023-03-03 18:48:23 +0000

    fixed script and made executable

--- linux.sh
+++ linux.sh
@@ -6,16 +6,18 @@ echo "Please enter your name:"
 read name
 echo "Reading.... please wait"
 sleep 3 
-echo "-----------------------------"
-echo "Your username is:" && whoami 
-echo "-----------------------------"
+echo "--------------------------------------------------------"
+echo "Your username is:"&& whoami 
+echo "--------------------------------------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
-echo "Please wait while we process the date......."
+echo "Please wait while we process the date..................."
 sleep 3
-echo "Made on:" && date
-echo "-----------------------------"
+echo "Made on:"&& date
+echo "--------------------------------------------------------"
 sleep 2
-echo "My IP address is:" && curl ifconfig.me
-echo "-----------------------------"
+echo "My IP address is:"&& curl ifconfig.me
+echo  
+echo "--------------------------------------------------------"
 sleep 2
+
