c0dc8f393c386d2f2db3eb327681a071b393d4b7
Rotimi Sule <rotimisule24@outlook.com>
2023-04-03 16:36:39 +0000

    Rotimi's commit for week 4 project 1

--- linux.sh
+++ linux.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 echo "Please enter your name:"
-read name
+read Rotimi aka GLO
 echo "Reading.... please wait"
 sleep 3 
 echo "-----------------------------"
-echo "Your username is:" && whoami 
+echo "Your username is:" && GLOTIMI
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
-echo "Made on:" && date
+echo "Made on:" && APRIL 3rd 2023
 echo "-----------------------------"
 sleep 2
-echo "My IP address is:"
+echo "My IP address is:"https://github.com/rotimisule/LevelUpTeam
 echo "-----------------------------"
 sleep 2
 curl ifconfig.me
