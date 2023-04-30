be6a8935132a533524b31d51439acb306b2bccf1
AChandler07 <alexandriachan127@gmail.com>
2023-04-30 03:44:35 +0000

    I have made the necessary changes to this file

--- linux.sh
+++ linux.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 echo "Please enter your name:"
-read name
+read Alexandria Chandler
 echo "Reading.... please wait"
 sleep 3 
 echo "-----------------------------"
-echo "Your username is:" && whoami 
+echo "Your username is:" && AChandler07 
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
-echo "Made on:" && date
+echo "Made on:" && April_29_2023
 echo "-----------------------------"
 sleep 2
 echo "My IP address is:"
@@ -20,3 +20,4 @@ sleep 2
 curl ifconfig.me
 
 This is week 4 - project test
+Black Team all day!!!
